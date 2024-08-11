eeaf6502ae16f1d1e766b98ca85338d118db9152
richmondazadze <richmondazadze1313@gmail.com>
2024-08-11 01:21:29 +0000

    env updated

--- components/FirebaseAuth.js
+++ components/FirebaseAuth.js
@@ -12,12 +12,12 @@ import { useLanguage } from "../LanguageContext";
 import { translations } from "../translations";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDjXB8gTUQ33510-JqfZveFsz84zQtu0uI",
-  authDomain: "ai-customer-support-d34fe.firebaseapp.com",
-  projectId: "ai-customer-support-d34fe",
-  storageBucket: "ai-customer-support-d34fe.appspot.com",
-  messagingSenderId: "955040321821",
-  appId: "1:955040321821:web:31c92c1bde665f63a151f4",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
